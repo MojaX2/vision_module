763b127f98e6d7d540bf0aab3b26e0e66721386b
Kazuki MIyazawa <zawa0319@gmail.com>
2018-10-21 23:29:48 +0900

    fix docker network setting

--- docker/build-and-run.sh
+++ docker/build-and-run.sh
@@ -1,18 +1,21 @@
 IMAGE_NAME="moja2/vision_module"
 
+xhost +
+
 docker build -t $IMAGE_NAME .
 
 docker run ${RUN_ARGS} \
        -it --rm --name vision \
        --net="host" \
-       -e ROS_MASTER_URI=http://192.168.1.33:11311 \
-       -e ROS_IP=127.0.0.1 \
+       -e ROS_MASTER_URI=http://192.168.1.88:11311 \
+       -e ROS_IP=192.168.1.33 \
+       -e ROS_HOSTNAME="192.168.1.33" \
+       --add-host="octopus:192.168.1.33" \
+       --add-host="baxter.local:192.168.1.88" \
        -e DISPLAY=$DISPLAY \
-       -v $HOME/.Xauthority:/root/.Xauthority \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
-       -e QT_X11_NO_MITSHM=1 \
        -e LIBGL_ALWAYS_SOFTWARE=1 \
        -e HOME=/workspace \
        -v `pwd`/..:/catkin_ws/src/vision_module \
-       moja2/vision_module bash
-    #    moja2/vision_module bash -c "ln /dev/null /dev/raw1394; source /catkin_ws/devel/setup.bash; roslaunch vision_module  vision_module.launch"
+       -v `pwd`/../../vision_module_wrapper:/catkin_ws/src/vision_module_wrapper \
+       moja2/vision_module bash -c "ln /dev/null /dev/raw1394; source /catkin_ws/devel/setup.bash; roslaunch vision_module  vision.launch"
--- docker/d415_run.sh
+++ docker/d415_run.sh
@@ -1,6 +1,10 @@
 docker run --rm --net=host \
-            --privileged \
-            --volume=/dev:/dev \
-            --net="host" \
-            -it iory/docker-ros-d415:docker.d415 /bin/bash \
-            -i -c 'roslaunch realsense2_camera rs_rgbd.launch enable_pointcloud:=true align_depth:=false depth_registered_processing:=true align_depth:=true'
+        --privileged \
+        --volume=/dev:/dev \
+        --net="host" \
+        -e ROS_MASTER_URI=http://192.168.1.88:11311 \
+        -e ROS_HOSTNAME="192.168.1.33" \
+        --add-host="octopus:192.168.1.33" \
+        --add-host="baxter.local:192.168.1.88" \
+        -it --rm iory/docker-ros-d415:docker.d415 /bin/bash \
+        -i -c 'roslaunch realsense2_camera rs_rgbd.launch enable_pointcloud:=true align_depth:=false depth_registered_processing:=true align_depth:=true'
